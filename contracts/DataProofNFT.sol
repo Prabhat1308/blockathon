552ab19a32f97e2d905ee0a097c90b6501d287c3
Prabhat1308 <prabhatverma329@gmail.com>
2023-03-25 03:36:18 +0530

    added contract and deployed

--- contracts/DataProofNFT.sol
+++ contracts/DataProofNFT.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-contract Demo is ERC721URIStorage  {
+contract DataProofNFT is ERC721URIStorage  {
  using Counters for Counters.Counter;
  Counters.Counter private _tokenIds;
 
@@ -23,7 +23,4 @@ contract Demo is ERC721URIStorage  {
 
  }
  
-
-
-
 }
